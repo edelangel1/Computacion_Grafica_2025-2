81f7e06a8c32f961b61202bdd6c24390aae790b2
edelangel1 <eduardodelangel17@outlook.com>
2025-03-16 04:47:39 -0600

    Práctica 6

--- ConfigInicial/Carga de Modelos.cpp
+++ ConfigInicial/Carga de Modelos.cpp
@@ -1,6 +1,6 @@
 /*
-Carga de modelos
-Entrega: 11/03/2025
+Practica 6
+Entrega: 16/03/2025
 Perez Del Angel Joaquin Eduardo
 Numero de cuenta: 422090638
 */
@@ -29,7 +29,7 @@ Numero de cuenta: 422090638
 #include "stb_image.h"
 
 // Properties
-const GLuint WIDTH = 800, HEIGHT = 600;
+const GLuint WIDTH = 1200, HEIGHT = 650;
 int SCREEN_WIDTH, SCREEN_HEIGHT;
 
 // Function prototypes
@@ -57,7 +57,7 @@ int main( ){
     glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
     
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "Carga de modelos y camara sintetica - Eduardo", nullptr, nullptr );
+    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "Practica 6 - Eduardo", nullptr, nullptr );
     
     if ( nullptr == window ){
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -95,8 +95,12 @@ int main( ){
     Shader shader( "Shader/modelLoading.vs", "Shader/modelLoading.frag" );
     
     // Load models
-    Model dog((char*)"Models/RedDog.obj");
+    Model dog((char*)"Models/Perro/RedDog.obj");
     Model park((char*)"Models/Parque/Park.obj");
+    Model hydrant((char*)"Models/Hidrante/Hydrant.obj");
+    Model bench((char*)"Models/Banca/Bench.obj");
+    Model bicycle((char*)"Models/Bici/Bicycle.obj");
+    Model cat((char*)"Models/Gato/Cat.obj");
     glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( float )SCREEN_WIDTH/( float )SCREEN_HEIGHT, 0.1f, 100.0f );
 
     // Game loop
@@ -121,20 +125,51 @@ int main( ){
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
         // Draw the loaded model
-        // Dog
-        glm::mat4 modelDog = glm::mat4(1.0f);
-        modelDog = glm::translate(modelDog, glm::vec3(1.0f, -0.1f, 2.0f));
-        modelDog = glm::scale(modelDog, glm::vec3(0.5f, 0.5f, 0.5f));
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelDog));
-        dog.Draw(shader);
-
         // Park
         glm::mat4 modelPark = glm::mat4(1.0f);
         modelPark = glm::translate(modelPark, glm::vec3(1.0f, 0.0f, 0.0f));
         modelPark = glm::rotate(modelPark, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        modelPark = glm::scale(modelPark, glm::vec3(4.0f, 4.0f, 4.0f));
+        modelPark = glm::scale(modelPark, glm::vec3(10.0f, 10.0f, 10.0f));
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelPark));
         park.Draw(shader);
+
+        //// Dog
+        glm::mat4 modelDog = glm::mat4(1.0f);
+        modelDog = glm::translate(modelDog, glm::vec3(3.0f, -0.4f, 3.0f));
+        modelDog = glm::scale(modelDog, glm::vec3(1.0f, 1.0f, 1.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelDog));
+        dog.Draw(shader);
+
+        // Bench
+        glm::mat4 modelBench = glm::mat4(1.0f);
+        modelBench = glm::translate(modelBench, glm::vec3(0.5f, -0.5f, 0.4f));
+        modelBench = glm::rotate(modelBench, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        modelBench = glm::scale(modelBench, glm::vec3(1.5f, 1.5f, 1.5f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelBench));
+        bench.Draw(shader);
+
+        //// Hydrant
+        glm::mat4 modelHydrant = glm::mat4(1.0f);
+        modelHydrant = glm::translate(modelHydrant, glm::vec3(2.0f, -0.57f, -0.2f));
+        modelHydrant = glm::rotate(modelHydrant, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        modelHydrant = glm::scale(modelHydrant, glm::vec3(0.6f, 0.6f, 0.6f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelHydrant));
+        hydrant.Draw(shader);
+
+        //// Bicycle
+        glm::mat4 modelBicycle = glm::mat4(1.0f);
+        modelBicycle = glm::translate(modelBicycle, glm::vec3(-1.0f, -0.55f, 3.3f));
+        modelBicycle = glm::rotate(modelBicycle, glm::radians(40.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        modelBicycle = glm::scale(modelBicycle, glm::vec3(1.0f, 1.0f, 1.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelBicycle));
+        bicycle.Draw(shader);
+
+        // Cat
+        glm::mat4 modelCat = glm::mat4(1.0f);
+        modelCat = glm::translate(modelCat, glm::vec3(0.6f, -0.3f, 0.5f));
+        modelCat = glm::scale(modelCat, glm::vec3(0.5f, 0.5f, 0.5f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelCat));
+        cat.Draw(shader);
 
         // Swap the buffers
         glfwSwapBuffers( window );
